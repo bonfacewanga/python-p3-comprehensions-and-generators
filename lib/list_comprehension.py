c517b6fc39ad033bc3f8f309c336286e5fd25aff
Bonface <bonniewanga34@gmail.com>
2023-09-29 17:36:54 +0300

    Completed assignments

--- lib/list_comprehension.py
+++ lib/list_comprehension.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python3
 
 def return_evens(num_list):
-    pass
+    num_list = [ n for n in num_list if n % 2 == 0]
+    return num_list
 
 def make_exclamation(sentence_list):
-    pass
+    sentence_list = [sentence + '!' for sentence in sentence_list]
+    return sentence_list
